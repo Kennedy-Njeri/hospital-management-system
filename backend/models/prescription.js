823b9ad6c16aec16138fa9e9d9dee88bf8b95289
Kennedy Njeri <mistakenz123@gmail.com>
2021-02-10 16:52:33 +0300

    prescription controllers && models

---
+++ backend/models/prescription.js
@@ -0,0 +1,53 @@
+const mongoose =  require('mongoose')
+const { ObjectId } = mongoose.Schema;
+
+
+
+const Prescription = mongoose.Schema(
+    {
+        user: {
+            type: ObjectId,
+            required: true,
+            ref: 'User',
+        },
+        treatment: {
+            type: ObjectId,
+            required: true,
+            ref: 'Treatment',
+        },
+        medicine: {
+            type: String,
+            required: true,
+
+        },
+        time: {
+            type: Number,
+            required: true
+        },
+        days: {
+            type: Number,
+            required: true
+        },
+        take: {
+            type: String,
+            default: "Morning",
+            enum: ["Morning", "Midday", "Evening"] // enum means string objects
+        },
+        test: {
+            type: ObjectId,
+            required: true,
+            ref: 'testSchema',
+        },
+        history: {
+            type: String,
+            required: true,
+
+        }
+    }
+)
+
+
+
+
+
+module.exports = mongoose.model('Prescription', Prescription);
