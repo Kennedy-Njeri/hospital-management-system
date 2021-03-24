fe5d9d2efee0caea09e6fef112ab500d9cf48db4
Kennedy-Njeri <mistakenz123@gmail.com>
2021-03-24 20:26:21 +0300

    security with helmet

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,8 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require("helmet");
+
 require('./db/mongoose')
 const path = require('path')
 
@@ -41,6 +43,8 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(cors());
 app.use(mongoSanitize());
+app.use(helmet());
+
 
 
 // app.use(function(req, res, next) { //allow cross origin requests
